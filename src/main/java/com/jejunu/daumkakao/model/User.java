7a907a020ea614d563c40995074568de9e3776d7
moonsunhee1 <mshee0505@gmail.com>
2015-06-05 16:51:09 +0900

    User에서 필요한 값들 다시 설정

--- src/main/java/com/jejunu/daumkakao/model/User.java
+++ src/main/java/com/jejunu/daumkakao/model/User.java
@@ -1,39 +1,49 @@
 package com.jejunu.daumkakao.model;
 
 public class User {
+	private Integer id;
 	private String name;
-	private String price;
-	private String seller;
-	private String day;
+	private Integer price;
+	private Integer seller;
 	private String productinform;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getPrice() {
+
+	public Integer getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+
+	public void setPrice(Integer price) {
 		this.price = price;
 	}
-	public String getSeller() {
+
+	public Integer getSeller() {
 		return seller;
 	}
-	public void setSeller(String seller) {
+
+	public void setSeller(Integer seller) {
 		this.seller = seller;
 	}
-	public String getDay() {
-		return day;
-	}
-	public void setDay(String day) {
-		this.day = day;
-	}
+
 	public String getProductinform() {
 		return productinform;
 	}
+
 	public void setProductinform(String productinform) {
 		this.productinform = productinform;
-	}	
+	}
 }
